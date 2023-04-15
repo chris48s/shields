9c015a18d34135374b3f27b2256961fe590c494b
chris48s <git@chris-shaw.dev>
2023-04-15 16:31:20 +0100

    merge instead of using git apply

--- .github/scripts/deploy-review-app.sh
+++ .github/scripts/deploy-review-app.sh
@@ -12,7 +12,8 @@ pr_json=$(curl --fail "https://api.github.com/repos/badges/shields/pulls/$PR_NUM
 
 # Attempt to apply the PR diff to the target branch
 # This will fail if it does not merge cleanly
-curl --location "$(echo "$pr_json" | jq .patch_url -r)" | git apply
+git fetch origin "pull/$PR_NUMBER/head:pr-$PR_NUMBER"
+git merge "pr-$PR_NUMBER"
 
 # If the app does not already exist, create it
 if ! flyctl status --app "$app"; then
